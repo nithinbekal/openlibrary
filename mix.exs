e30afe554638f8146d2c7e5d64f0f447e2c220fd
Nithin Bekal <nithinbekal@gmail.com>
2016-11-24 01:14:13 +0530

    Remove comments from mix file

--- mix.exs
+++ mix.exs
@@ -10,9 +10,6 @@ defmodule Openlibrary.Mixfile do
      deps: deps()]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
     [applications: [
       :httpoison,
@@ -22,15 +19,6 @@ defmodule Openlibrary.Mixfile do
      mod: {Openlibrary, []}]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
     [
       {:httpoison, "~> 0.10"},
