ecae3a39fe42ee279df9a9359917c3a9c7f4df27
Nithin Bekal <nithinbekal@gmail.com>
2017-05-06 15:16:10 +0530

    Bump version to 0.1.1

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Openlibrary.Mixfile do
 
   def project do
     [app: :openlibrary,
-     version: "0.1.0",
+     version: "0.1.1",
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
