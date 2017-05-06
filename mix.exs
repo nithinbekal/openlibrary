4087d007e1d1ecde276a8c24940e5474aed667b9
Nithin Bekal <nithinbekal@gmail.com>
2017-05-06 13:15:56 +0530

    Add parentheses to function calls

--- mix.exs
+++ mix.exs
@@ -7,9 +7,9 @@ defmodule Openlibrary.Mixfile do
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps,
+     deps: deps(),
      description: "Elixir client for Open Library REST API",
-     package: package,
+     package: package(),
     ]
   end
 
