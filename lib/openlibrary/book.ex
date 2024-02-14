6dd5a420543255866be19ae6d37ffce5ae2df193
Nithin Bekal <nithinbekal@gmail.com>
2024-02-13 22:22:41 -0500

    Add Book.find_by_isbns/1

--- lib/openlibrary/book.ex
+++ lib/openlibrary/book.ex
@@ -28,13 +28,42 @@ defmodule Openlibrary.Book do
   end
 
   @doc """
+  Fetches book information for a list of ISBNs. Returns a map where the keys are
+  the ISBNs and the values are the corresponding book information.
+
+  The function constructs a single URL with all the ISBNs and makes a single request
+  to the OpenLibrary API.
+
+  ## Examples
+
+      > Openlibrary.Book.find_by_isbns(["0812511816", "0451524934"])
+      # %{
+      #   "ISBN:0812511816" => %{ title: "The Eye of the World", authors: [%{}, %{}], ... },
+      #   "ISBN:0451524934" => %{ title: "1984", authors: [%{}, %{}], ... }
+      # }
+
+      > Openlibrary.Book.find_by_isbns(["invalidisbn", "isbn not present in db"])
+      # %{
+      #   "ISBN:invalidisbn" => nil,
+      #   "ISBN:isbn not present in db" => nil
+      # }
+
+  """
+  def find_by_isbns(isbns) do
+    isbns
+    |> Enum.map(fn isbn -> "ISBN:#{isbn}" end)
+    |> Enum.join(",")
+    |> find_by_bibkeys()
+  end
+
+  @doc """
   Fetch book information using Library of Congress catalog number.
 
       > Openlibrary.Book.find_by_lccn("lccn")
       # %{ title: "The Eye of the World", authors: [%{}, %{}], ... }
 
   """
-  def find_by_lccn(lccn) do 
+  def find_by_lccn(lccn) do
     find_by_bibkey("LCCN:#{lccn}")
   end
 
@@ -45,16 +74,20 @@ defmodule Openlibrary.Book do
       # %{ title: "The Eye of the World", authors: [%{}, %{}], ... }
 
   """
-  def find_by_oclc(oclc) do 
+  def find_by_oclc(oclc) do
     find_by_bibkey("OCLC:#{oclc}")
   end
 
-  # Returns a map after fetching book information for a given key, which can be
-  # ISBN, LCCN, or OCLC.
-  defp find_by_bibkey(bibkey) do 
+  defp find_by_bibkey(bibkey) do
+    # bibkey can be ISBN, LCCN, or OCLC.
     "#{@api_url}/books?bibkeys=#{bibkey}&jscmd=data&format=json"
     |> fetch_json()
     |> Map.get(bibkey)
+  end
+
+  defp find_by_bibkeys(bibkeys) do
+    "#{@api_url}/books?bibkeys=#{bibkeys}&jscmd=data&format=json"
+    |> fetch_json()
   end
 
   defp fetch_json(url) do
