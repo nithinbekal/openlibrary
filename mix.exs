2bc69587688fabda1a8f26217ddc6a747a3c6d8d
Nithin Bekal <nithinbekal@gmail.com>
2024-02-13 22:37:03 -0500

    Bump version to 0.2.1

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Openlibrary.Mixfile do
 
   def project do
     [app: :openlibrary,
-     version: "0.2.0",
+     version: "0.2.1",
      elixir: "~> 1.12",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
