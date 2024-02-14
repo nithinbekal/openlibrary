27e8bca794921f6679045138592d8a140a6c1816
Nithin Bekal <nithinbekal@gmail.com>
2024-02-13 22:31:51 -0500

    Update elixir version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Openlibrary.Mixfile do
   def project do
     [app: :openlibrary,
      version: "0.1.1",
-     elixir: "~> 1.3",
+     elixir: "~> 1.16",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps(),
